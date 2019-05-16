e77ec7162f060c676cfd478427d32efb9bb0108d
wanaka <asd8116@gmail.com>
2019-05-16 23:59:48 +0800

    Add login modifying config passport

--- config/passport.js
+++ config/passport.js
@@ -7,14 +7,13 @@ const User = db.User
 module.exports = passport => {
   passport.use(
     new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-      User.findOne({
-        email: email
-      }).then(user => {
+      User.findOne({ where: { email: email } }).then(user => {
         if (!user) {
           return done(null, false, { message: 'That email is not registered' })
         }
 
         if (user.password != password) {
+          console.log('user password not correct.')
           return done(null, false, { message: 'Email or Password incorrect' })
         }
         return done(null, user)
@@ -70,8 +69,8 @@ module.exports = passport => {
     done(null, user.id)
   })
   passport.deserializeUser((id, done) => {
-    User.findByPk(id, (err, user) => {
-      done(err, user)
+    User.findByPk(id).then(user => {
+      done(null, user)
     })
   })
 }
