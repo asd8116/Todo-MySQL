c02e5c0e8a1cd373b164f76914a886966ee66765
wanaka <asd8116@gmail.com>
2019-05-24 19:04:50 +0800

    Modify passport errors

--- config/passport.js
+++ config/passport.js
@@ -6,12 +6,12 @@ const User = db.User
 
 module.exports = passport => {
   passport.use(
-    new LocalStrategy({ usernameField: 'email' }, async (email, passport, done) => {
+    new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
       const user = await User.findOne({ where: { email: email } })
 
       if (!user) return done(null, false, { message: 'That email is not registered' })
 
-      const isMatch = await bcrypt.compare(passport, user.password)
+      const isMatch = await bcrypt.compare(password, user.password)
 
       if (isMatch) return done(null, user)
       else return done(null, false, { message: 'Email and Password incorrect' })
@@ -36,8 +36,7 @@ module.exports = passport => {
           .toString(36)
           .slice(-8)
 
-        const salt = await bcrypt.genSalt(10)
-        const hash = await bcrypt.hash(randomPassword, salt)
+        const hash = bcrypt.hashSync(randomPassword, 10)
 
         const newUser = new User({ name, email, password: hash })
         await newUser.save()
